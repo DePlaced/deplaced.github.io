c76d72e059cd80c34469206fb06b3167306e06b2
paapladspjuske@gmail.com <10414028@ucn.dk>
2024-06-07 13:34:23 +0200

    updaterd with nav bar

--- deplace-tech/src/app/app.component.ts
+++ deplace-tech/src/app/app.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+
+  
 }
--- deplace-tech/src/app/app.module.ts
+++ deplace-tech/src/app/app.module.ts
@@ -1,21 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 //Angular material
-import {MatButtonModule} from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { ContactComponent } from './contact/contact.component';
+
+//Components
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MatSidenavModule,
+    MatToolbarModule,
     MatButtonModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [
     provideAnimationsAsync()
