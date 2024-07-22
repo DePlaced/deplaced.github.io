84d2b561d7751cb9dd3bb968847a57d99023729d
DePlaced <paapladspjuske@gmail.com>
2024-07-22 23:48:23 +0200

    added flicker

--- deplace-tech/src/app/services/type-writer.service.ts
+++ deplace-tech/src/app/services/type-writer.service.ts
@@ -1,5 +1,5 @@
-import { Injectable, Type } from '@angular/core';
-import { concat, concatMap, delay, from, ignoreElements, interval, map, of, repeat, take } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { concat, concatMap, delay, from, interval, map, merge, of, scan, take, withLatestFrom } from 'rxjs';
 
 interface TypeParams {
   word: string;
@@ -21,14 +21,26 @@ export class TypeWriterService {
   typeEffect(word: string) {
     return concat(
       this.type({ word, speed: 50 }),
-      of('').pipe(delay(1200), ignoreElements()),
+      of('').pipe(delay(1200)),
     );
   }
 
   private type({ word, speed, backwards = false }: TypeParams) {
-    return interval(speed).pipe(
+    const typing$ = interval(speed).pipe(
       map(x => backwards ? word.substring(0, word.length - x) : word.substring(0, x + 1)),
       take(word.length)
     );
-}
+
+    const flicker$ = interval(speed * 2).pipe(
+      map(x => x % 2 === 0 ? '' : '_')
+    );
+
+    return merge(
+      typing$.pipe(map(typed => ({ typed }))),
+      flicker$.pipe(map(flicker => ({ flicker })))
+    ).pipe(
+      scan((acc, curr) => ({ ...acc, ...curr }), { typed: '', flicker: '' }),
+      map(({ typed, flicker }) => typed + flicker)
+    );
+  }
 }
