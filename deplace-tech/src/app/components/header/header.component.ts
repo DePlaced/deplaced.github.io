aff9b26285f4c5318f3864a0a3ae46361d2a940f
DePlaced <paapladspjuske@gmail.com>
2024-07-22 23:12:01 +0200

    removed wrongly placed mobile header and added logo

--- deplace-tech/src/app/components/header/header.component.ts
+++ deplace-tech/src/app/components/header/header.component.ts
@@ -2,9 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { Observable } from 'rxjs';
-import { ScreenSizeService } from '../../services/screen-size.service';
-import { AsyncPipe, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-header',
@@ -14,23 +11,11 @@ import { AsyncPipe, NgIf } from '@angular/common';
     MatIconModule,
     RouterLink,
     RouterLinkActive,
-    AsyncPipe,
-    NgIf
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent implements OnInit {
-  isMobile$: Observable<boolean>;
-  mobileMenuActive = false;
-
-  constructor(private screenSizeService: ScreenSizeService) {
-    this.isMobile$ = this.screenSizeService.isMobile$;
-  }
 
   ngOnInit(): void {}
-
-  toggleMobileMenu() {
-    this.mobileMenuActive = !this.mobileMenuActive;
-  }
 }
